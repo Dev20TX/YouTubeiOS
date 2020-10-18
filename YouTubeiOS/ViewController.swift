067e28aad6d2bdda44d839e12cfa701b7a7f9f4b
Ryan Bennett <ryanbennett@Ryans-MacBook-Pro.local>
2020-10-18 00:08:55 -0500

    initial xcode project

---
+++ YouTubeiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeiOS
+//
+//  Created by Ryan Bennett on 10/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
